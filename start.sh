858197c75dd8b67c2e89a4ce6da34a10d705208c
Sophie Hasindrae <sophiehasindrae@gmail.com>
2025-02-10 16:47:20 +0300

    ajout ligne de debogage

--- start.sh
+++ start.sh
@@ -5,3 +5,10 @@ php-fpm
 
 # Start Nginx
 nginx -g 'daemon off;'
+
+# Check Nginx status
+sleep 5
+if ! pgrep nginx > /dev/null; then
+    echo "Nginx failed to start......................."
+    exit 1
+fi
